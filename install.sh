91f73ba7d9d3739dce1aec8acc52e0da6b707c64
Botond Fasanga <47340171+botondf@users.noreply.github.com>
2025-09-29 22:35:56 -0400

    Create install.sh
    first time install. must be in $HOME and the git repo dir $HOME/termux-url-opener must exist. make and link to bin, setup storage if not done already

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/data/data/com.termux/files/usr/bin/bash              
+
+# An initial install script. Only run the first time.
+if [ "$(pwd)" != "$HOME" ]; then
+    echo "Please run from $HOME."
+    exit 1
+elif [ ! -d termux-url-opener ]; then
+    echo "could not find $HOME/termux-url-opener. Was this cloned in the wrong directory?"
+    exit 1
+fi
+
+cd $HOME
+mkdir bin
+ln -s $HOME/termux-url-opener/termux-url-opener.sh $HOME/bin/termux-url-opener.sh
+ln -s $HOME/termux-url-opener/termux-url-opener.sh $HOME/bin/termux-file-editor.sh
+termux-setup-storage
+dir=/storage/emulated/0/Videos
+mkdir -p $dir
+
+echo "Sucessfully installed to $HOME/bin/termux-url-opener.sh. Files are downloaded to $dir.
