d544ef8b99b27acc5d84e5dd893d3ca698d0b26f
Botond Fasanga <47340171+botondf@users.noreply.github.com>
2025-09-29 22:33:47 -0400

    Update termux-url-opener.sh
    check for dependencies; added variable for and changed the download directory.

--- termux-url-opener.sh
+++ termux-url-opener.sh
@@ -1,4 +1,17 @@
 #!/data/data/com.termux/files/usr/bin/bash              
+
+dir=/storage/emulated/0/Videos
+
+# Check for Termux dependencies
+for PACKAGE in ffmpeg yt-dlp 
+do
+	which $PACKAGE > /dev/null
+	if [ ! $? -eq 0 ]; then
+		echo "Installing $PACKAGE"
+		pkg install $PACKAGE
+	fi
+done
+
 url=$1
 echo "2) lower res (240p)"
 echo "3) low res (360p)"
@@ -9,16 +22,16 @@ echo "q) quit"
 read CHOICE
 case $CHOICE in
   2)
-    yt-dlp -o "/storage/emulated/0/Movies/%(title)s.%(ext)s" $url -S res:240
+    yt-dlp -o "$dir/%(title)s.%(ext)s" $url -S res:240
     ;;
   3)
-    yt-dlp -o "/storage/emulated/0/Movies/%(title)s.%(ext)s" $url -S res:360
+    yt-dlp -o "$dir/%(title)s.%(ext)s" $url -S res:360
     ;;                                              
   4)
-    yt-dlp -o "/storage/emulated/0/Movies/%(title)s.%(ext)s" $url -S res:480
+    yt-dlp -o "$dir/%(title)s.%(ext)s" $url -S res:480
     ;;
   x)
-    yt-dlp -o "/storage/emulated/0/Movies/%(title)s.%(ext)s" $url -x --audio-format mp3
+    yt-dlp -o "$dir/%(title)s.%(ext)s" $url -x --audio-format mp3
     ;;                                              
   q)
     ;;
