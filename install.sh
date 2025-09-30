e92478a4e673f737661fd56d119e26c87c20620b
Botond Fasanga <47340171+botondf@users.noreply.github.com>
2025-09-29 23:04:58 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/data/data/com.termux/files/usr/bin/bash              
 
 # An initial install script. Only run the first time.
-if [ "$(pwd)" != "$HOME" ]; then
-    echo "Please run from $HOME."
+
+wd="$HOME/termux-url-opener"
+
+if [ "$(pwd)" != "$wd" ]; then
+    echo "Please run inside $wd."
     exit 1
-elif [ ! -d termux-url-opener ]; then
-    echo "could not find $HOME/termux-url-opener. Was this cloned in the wrong directory?"
+elif [ ! -e install.sh ]; then
+    echo "could not find install.sh. Please run inside $wd."
     exit 1
 fi
 
